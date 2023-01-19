4109b87f8d8c29064148b46e13d79863ac1ec3c3
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-01-19 20:39:43 +0200

    body

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
 body {
 
 
-  
+  padding-top:20px;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
